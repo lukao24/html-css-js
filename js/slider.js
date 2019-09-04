196b8de200e20b1c0c161b414f4d1d27c2f89f3d
Lucas Oliveira <olucas259@gmail.com>
2019-09-04 20:51:32 -0300

    style.css

---
+++ js/slider.js
@@ -0,0 +1,22 @@
+
+
+var slides = ["#s1", "#s2", "#s3", "#s4", "#s5"];
+slideatual = 0;
+slidemax = 4;
+tempo = 10000;
+
+$(document).ready(function () {
+    $(slides[slideatual]).fadeTo(1000, 1);
+    setInterval(troca, tempo);
+    function troca() {
+        $(slides[slideatual]).fadeTo(1000, 0);
+        slideatual++;
+        if (slideatual > slidemax) {
+            slideatual = 0;
+        }
+        $(slides[slideatual]).fadeTo(1000, 1);
+    }
+});
+
+
+
